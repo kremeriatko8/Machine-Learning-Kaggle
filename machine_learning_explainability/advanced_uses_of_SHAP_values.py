c37adec437a63f8088f471a3516d6567e65ad5cf
kremeriatko8 <sofia.kremer08@gmail.com>
2026-05-08 16:11:30 +0300

    The last stage of this course is simply a reflection and application of everything on a case study with a hospital, answering questions and creating graphs.

---
+++ machine_learning_explainability/advanced_uses_of_SHAP_values.py
@@ -0,0 +1,84 @@
+'''explanation'''
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv('../input/fifa-2018-match-statistics/FIFA 2018 Statistics.csv')
+y = (data['Man of the Match'] == "Yes")  # Convert from string "Yes"/"No" to binary
+feature_names = [i for i in data.columns if data[i].dtype in [np.int64, np.int64]]
+X = data[feature_names]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+my_model = RandomForestClassifier(random_state=0).fit(train_X, train_y)
+
+import shap  # package used to calculate Shap values
+
+# Create object that can calculate shap values
+explainer = shap.TreeExplainer(my_model)
+
+# calculate shap values. This is what we will plot.
+# Calculate shap_values for all of val_X rather than a single row, to have more data for plot.
+shap_values = explainer.shap_values(val_X)
+
+# Make plot. Index of [1] is explained in text below.
+shap.summary_plot(shap_values[1], val_X)
+
+import shap  # package used to calculate Shap values
+
+# Create object that can calculate shap values
+explainer = shap.TreeExplainer(my_model)
+
+# calculate shap values. This is what we will plot.
+shap_values = explainer.shap_values(X)
+
+# make plot.
+shap.dependence_plot('Ball Possession %', shap_values[1], X, interaction_index="Goal Scored")
+
+#=-=--==-=-=-=-=--===--=-==-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=-=-=-=--==-=-=-=-=-
+#my codeeeeeeee
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import shap
+
+# Environment Set-Up for feedback system.
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_explainability.ex5 import *
+print("Setup Complete")
+
+
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('../input/hospital-readmissions/train.csv')
+y = data.readmitted
+base_features = ['number_inpatient', 'num_medications', 'number_diagnoses', 'num_lab_procedures',
+                 'num_procedures', 'time_in_hospital', 'number_outpatient', 'number_emergency',
+                 'gender_Female', 'payer_code_?', 'medical_specialty_?', 'diag_1_428', 'diag_1_414',
+                 'diabetesMed_Yes', 'A1Cresult_None']
+
+# Some versions of shap package error when mixing bools and numerics
+X = data[base_features].astype(float)
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# For speed, we will calculate shap values on smaller subset of the validation data
+small_val_X = val_X.iloc[:150]
+my_model = RandomForestClassifier(n_estimators=30, random_state=1).fit(train_X, train_y)
+
+explainer = shap.TreeExplainer(my_model)
+shap_values = explainer.shap_values(small_val_X)
+
+shap.summary_plot(shap_values[1], small_val_X)
+
+# set following variable to 'diag_1_428' or 'payer_code_?'
+feature_with_bigger_range_of_effects = 'diag_1_428'
+
+# Check your answer
+q_1.check()
+# Correct
+
+#there were just questions on plots so i don`t paste them here.
